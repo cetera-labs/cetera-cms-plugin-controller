5cb9c0231291b2d4b8c115ea4f7c98e6dfd22536
Roman Romanov <nicodim@mail.ru>
2019-04-30 15:06:55 +0300

    initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @license MIT
+ */
+
+namespace Cetera\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // Register the plugin installer
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
