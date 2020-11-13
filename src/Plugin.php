f47dd6e3d462f19dc7263e1b3807a6d0268cf99a
Roman Romanov <nicodim@mail.ru>
2020-11-13 16:26:14 +0300

    2.0

--- src/Plugin.php
+++ src/Plugin.php
@@ -23,4 +23,10 @@ class Plugin implements PluginInterface
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+    
+    public function deactivate(Composer $composer, IOInterface $io)
+    {}
+    
+    public function uninstall(Composer $composer, IOInterface $io)
+    {}
 }
